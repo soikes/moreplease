0f834223918bae94db33406eb98ed51231b8e13a
Michael Soikkeli <mike@soikke.li>
2025-01-13 11:22:10 -0800

    checkpoint: start hardening web server for production use

--- pkg/web/server.go
+++ pkg/web/server.go
@@ -1,16 +1,58 @@
 package web
 
-// import "net/http"
+import (
+	"crypto/tls"
+	"net/http"
+	"time"
+)
 
-// func NewServer() *http.Server {
+func NewServer() *http.Server {
+	tc := &tls.Config{
+		PreferServerCipherSuites: true,
+		CurvePreferences: []tls.CurveID{
+			tls.CurveP256,
+			tls.X25519,
+		},
+		MinVersion: tls.VersionTLS12,
+		CipherSuites: []uint16{
+			tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
+			tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+			tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305, // Go 1.8 only
+			tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305,   // Go 1.8 only
+			tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+			tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
 
-// }
+			// Best disabled, as they don't provide Forward Secrecy,
+			// but might be necessary for some clients
+			// tls.TLS_RSA_WITH_AES_256_GCM_SHA384,
+			// tls.TLS_RSA_WITH_AES_128_GCM_SHA256,
+		},
+	}
+	srv := &http.Server{
+		ReadHeaderTimeout: 5 * time.Second,
+		ReadTimeout:       10 * time.Second,
+		WriteTimeout:      10 * time.Second,
+		IdleTimeout:       120 * time.Second,
+		TLSConfig:         tc,
+	}
+	return srv
+}
 
-// func SecurityHeaders() func(next http.Handler) http.Handler {
-// 	return func(next http.Handler) http.Handler {
-// 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-// 			w.Header().Set("X-Frame-Options", "DENY") // Warning: Instead of this header, use the frame-ancestors directive in a Content-Security-Policy header.
-// 			w.Header().Set("X-Content-Type-Options", "nosniff")
-// 		})
-// 	}
-// }
+const CSPString = ``
+
+func SecurityHeaders() func(next http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.Header().Set("X-Content-Type-Options", "nosniff")
+			w.Header().Set("Referrer-Policy", "strict-origin-when-cross-origin")
+			w.Header().Set("Strict-Transport-Security", "max-age=86400; includeSubDomains")
+			w.Header().Set("Content-Security-Policy", "")
+			w.Header().Set("Cross-Origin-Opener-Policy", "same-origin")
+			w.Header().Set("Cross-Origin-Embedder-Policy", "require-corp")
+			w.Header().Set("Cross-Origin-Resource-Policy", "same-site")
+			w.Header().Set("Permissions-Policy", "geolocation=(), camera=(), microphone=()")
+			w.Header().Set("Permissions-Policy", "interest-cohort=()")
+			w.Header().Set("Server", "webserver")
+		})
+	}
+}
